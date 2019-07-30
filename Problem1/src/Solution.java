695d1e50ab14a08f1e36252a0daaf3667f678a35
Akarsh Kumar <akarshkumar0101@gmail.com>
2019-07-29 23:28:57 -0500

    Problem 1 done

---
+++ Problem1/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Solution {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valueToIndices = new HashMap<>();
+        for(int i=0 ; i<nums.length;i++){
+            int val = nums[i];
+            int reqVal = target - val;
+            if(valueToIndices.containsKey(reqVal)) {
+                return new int[]{valueToIndices.get(reqVal), i};
+            }
+            valueToIndices.put(nums[i], i);
+        }
+
+        return null;
+    }
+    /*
+        Given nums = [2, 7, 11, 15], target = 9,
+
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+     */
+    public static void main(String... args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ans = new Solution().twoSum(nums,target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+}
